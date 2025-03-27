77ca1605bf7f85d16d6cfc0f1f8e2237c5918018
Jonas Gesch <jonas.gesch@gmail.com>
2025-03-27 18:48:27 +0800

    Add unit tests for TextNode class

---
+++ src/test_textnode.py
@@ -0,0 +1,32 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_type_ne(self):
+        node = TextNode("This is a text node", TextType.ITALIC)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_text_ne(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is text node2", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_url_ne(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD, "www.test.com")
+        self.assertNotEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("test", TextType.NORMAL, "www.abc.com")
+        self.assertEqual(repr(node), "TextNode(test, normal, www.abc.com)")
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+# Run all unit tests for python modules
+python3 -m unittest discover -s src
