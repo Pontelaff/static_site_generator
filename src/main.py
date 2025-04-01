32d5f7572ec734f85458e378d2b3d96b86dfe460
Jonas Gesch <jonas.gesch@gmail.com>
2025-04-01 17:56:44 +0800

    Add function to copy directory contends

--- src/main.py
+++ src/main.py
@@ -1,11 +1,34 @@
 #!/usr/bin/env python3
 
 import sys
-from textnode import TextNode, TextType
+import os
+import shutil
+
+
+def copy_dir(src_dir: str, dst_dir: str) -> None:
+    # Check if source directory exists
+    if not os.path.isdir(src_dir):
+        raise IsADirectoryError(f"no such directory: '{src_dir}'")
+    # Create empty destination directory
+    if os.path.exists(dst_dir):
+        shutil.rmtree(dst_dir)
+    os.mkdir(dst_dir)
+
+    # Copy directory content recursively
+    dir_content = os.listdir(src_dir)
+    for name in dir_content:
+        src_path = os.path.join(src_dir, name)
+        dst_path = os.path.join(dst_dir, name)
+        if os.path.isdir(src_path):
+            if not os.path.exists(dst_path):
+                # Create directory, if not existing
+                os.mkdir(dst_path)
+            copy_dir(src_path, dst_path)
+        if os.path.isfile(src_path):
+            shutil.copyfile(src_path, dst_path)
 
 def main() -> int:
-    print(TextNode("yo I'm a node", TextType.PLAIN, "https://youtu.be/dQw4w9WgXcQ?si=_Uy8eYDTiVCcaA0j"))
-
+    copy_dir("static/", "public/")
     return 0
 
 if __name__ == "__main__":
